71464f10d61a81eaf373cad7d68ce37efe220dcf
Faidon Kotsakis <fdkots@gmail.com>
2026-04-04 16:56:37 +0300

    dockerfile dashboard

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -1,8 +1,8 @@
-!pip install streamlit
 import streamlit as st
 import requests
 import psycopg2
 import pandas as pd
 import os
 st.set_page_config(page_title="ParisLens Dashboard", layout="wide")
-st.title("🏡 ParisLens Monitoring Dashboard")
+st.title("ParisLens Monitoring Dashboard")
+
