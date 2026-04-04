b392f78815a3202bc4684e5173b1efede8fb3c4f
Faidon Kotsakis <fdkots@gmail.com>
2026-04-04 19:17:36 +0300

    Dashboard now displays the status of endpoint /health and the verification of our db

--- src/dashboard/app.py
+++ src/dashboard/app.py
@@ -6,3 +6,28 @@ import os
 st.set_page_config(page_title="ParisLens Dashboard", layout="wide")
 st.title("ParisLens Monitoring Dashboard")
 
+#chedck the API health 
+st.header("API STATUS") 
+try: 
+    res= requests.get("http://api:8000/health", timeout=10)
+    if res.status_code == 200: # if the API is healthy 
+        st.success("OK")
+    else: 
+        st.warning("API has some issues") 
+except Exception as e: 
+    st.error("Issue with API endpoint, cannot be reached") 
+         
+
+#check the scraper health: 
+st.header("Scraper Health")
+def get_scraper_logs():
+    db_url = os.environ.get("DATABASE_URL")
+    conn = psycopg2.connect(db_url)
+    df = pd.read_sql("SELECT * FROM scraper_runs ORDER BY start_time DESC LIMIT 10", conn)
+    conn.close()
+    return df
+try:
+    df_logs = get_scraper_logs()
+    st.dataframe(df_logs)
+except Exception as e:
+    st.error(f"Could not connect to database to fetch logs: {e}")
